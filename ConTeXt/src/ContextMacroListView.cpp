b80f1f068faafa4ed02c8f9b2e4be802bc78f790
unknown <luigi.scarso@gmail.com>
2018-01-29 12:24:28 +0100

    position the caret at the insertion point of the text.

--- ConTeXt/src/ContextMacroListView.cpp
+++ ConTeXt/src/ContextMacroListView.cpp
@@ -892,6 +892,12 @@ ContextMacroListView::getUserMacroSelected()
 			if (sstart == send)
 			{
 				SendMessage(hSci, SCI_ADDTEXT, lenMbcs, reinterpret_cast<LPARAM>(_s));
+				if (UserMacro[UserMacroIndexSelected][3].size()>0)
+				{
+					int pos = SendMessage(hSci, SCI_GETCURRENTPOS, 0, 0) - UserMacro[UserMacroIndexSelected][3].size();;
+					SendMessage(hSci, SCI_GOTOPOS, pos, 0);
+
+				}
 			}
 			else
 			{
